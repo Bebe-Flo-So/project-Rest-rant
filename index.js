ebb8ade3be06059be4dcb85667956db25a530195
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-05-23 00:29:23 -0400

    Adding places view with mock data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const app = express ()
+const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -8,12 +8,12 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('error404')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-
+  res.render('error404')
 })
+
 
 app.listen(process.env.PORT)
